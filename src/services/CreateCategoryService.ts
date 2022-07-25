d46b95c3b77123b8e417d7541ac30f31c4d2771b
matheus6p <mhpaes97@gmail.com>
2022-07-25 10:08:51 -0300

    aplicando SOLID

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,11 +1,11 @@
-import { CategoriesRepository } from "./../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 interface IRequest {
   name: string;
   description: string;
 }
 
 export class CreateCategoryService {
-  constructor(private categoriesRepository: CategoriesRepository) {}
+  constructor(private categoriesRepository: ICategoriesRepository) {}
   execute({ name, description }: IRequest): void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
 
